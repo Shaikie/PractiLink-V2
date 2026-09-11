d487affa3036b46525a42b2f7b5356839172ec43
NOFACE_NOMAD <shaikielyn16@gmail.com>
2026-09-11 09:52:21 +0300

    feat: add organization administration permission

--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -9,19 +9,10 @@ class RolePermissionSeeder extends Seeder
 {
     public function run(): void
     {
-        $permissions = [
-            ['name'=>'Manage users','slug'=>'users.manage'],
-            ['name'=>'Manage students','slug'=>'students.manage'],
-            ['name'=>'Manage application windows','slug'=>'applications.manage'],
-            ['name'=>'Review applications','slug'=>'applications.review'],
-            ['name'=>'Manage workflows','slug'=>'workflows.manage'],
-            ['name'=>'Manage placements','slug'=>'placements.manage'],
-            ['name'=>'Manage reports','slug'=>'reports.manage'],
-        ];
+        $permissions=[['name'=>'Manage users','slug'=>'users.manage'],['name'=>'Manage students','slug'=>'students.manage'],['name'=>'Manage application windows','slug'=>'applications.manage'],['name'=>'Review applications','slug'=>'applications.review'],['name'=>'Manage workflows','slug'=>'workflows.manage'],['name'=>'Manage organizations','slug'=>'organizations.manage'],['name'=>'Manage placements','slug'=>'placements.manage'],['name'=>'Manage reports','slug'=>'reports.manage']];
         foreach($permissions as $permission) DB::table('permissions')->updateOrInsert(['slug'=>$permission['slug']],array_merge($permission,['created_at'=>now(),'updated_at'=>now()]));
         $roles=[['name'=>'Administrator','slug'=>'administrator','description'=>'Full system access.'],['name'=>'Placement Officer','slug'=>'placement-officer','description'=>'Manages student placement activities.'],['name'=>'Supervisor','slug'=>'supervisor','description'=>'Supervises student training.']];
         foreach($roles as $role) DB::table('roles')->updateOrInsert(['slug'=>$role['slug']],array_merge($role,['created_at'=>now(),'updated_at'=>now()]));
-        $administrator=DB::table('roles')->where('slug','administrator')->value('id');
-        foreach(DB::table('permissions')->pluck('id') as $permissionId) DB::table('role_permissions')->updateOrInsert(['role_id'=>$administrator,'permission_id'=>$permissionId]);
+        $administrator=DB::table('roles')->where('slug','administrator')->value('id'); foreach(DB::table('permissions')->pluck('id') as $permissionId) DB::table('role_permissions')->updateOrInsert(['role_id'=>$administrator,'permission_id'=>$permissionId]);
     }
 }
