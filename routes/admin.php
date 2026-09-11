c64583597b6666459c98b3f0b1a9a596c8f2aa84
NOFACE_NOMAD <shaikielyn16@gmail.com>
2026-09-11 09:48:49 +0300

    feat: add placement letter routes

--- routes/admin.php
+++ routes/admin.php
@@ -3,13 +3,13 @@
 use App\Http\Controllers\AdminApplicationController;
 use App\Http\Controllers\AdminApplicationWindowController;
 use App\Http\Controllers\AdminPlacementController;
+use App\Http\Controllers\AdminPlacementLetterController;
 use App\Http\Controllers\AdminWorkflowController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('permission:applications.manage')->group(function () {
     Route::resource('application-windows', AdminApplicationWindowController::class)->except(['show'])->parameters(['application-windows'=>'applicationWindow'])->names('application-windows');
 });
-
 Route::middleware('permission:applications.review')->group(function () {
     Route::get('applications', [AdminApplicationController::class,'index'])->name('applications.index');
     Route::get('applications/{application}', [AdminApplicationController::class,'show'])->name('applications.show');
@@ -18,7 +18,6 @@ Route::middleware('permission:applications.review')->group(function () {
     Route::post('applications/{application}/return', [AdminApplicationController::class,'returnApplication'])->name('applications.return');
     Route::post('applications/{application}/reject', [AdminApplicationController::class,'reject'])->name('applications.reject');
 });
-
 Route::middleware('permission:workflows.manage')->prefix('workflows')->name('workflows.')->group(function () {
     Route::get('/', [AdminWorkflowController::class,'index'])->name('index');
     Route::post('/', [AdminWorkflowController::class,'store'])->name('store');
@@ -27,10 +26,11 @@ Route::middleware('permission:workflows.manage')->prefix('workflows')->name('wor
     Route::put('/versions/{version}', [AdminWorkflowController::class,'updateVersion'])->name('versions.update');
     Route::post('/versions/{version}/publish', [AdminWorkflowController::class,'publish'])->name('versions.publish');
 });
-
 Route::middleware('permission:placements.manage')->prefix('placements')->name('placements.')->group(function () {
     Route::get('/', [AdminPlacementController::class,'index'])->name('index');
     Route::get('/applications/{application}/create', [AdminPlacementController::class,'create'])->name('create');
     Route::post('/applications/{application}', [AdminPlacementController::class,'store'])->name('store');
     Route::put('/{placement}/status', [AdminPlacementController::class,'updateStatus'])->name('status.update');
+    Route::post('/{placement}/letter', [AdminPlacementLetterController::class,'issue'])->name('letter.issue');
+    Route::get('/letters/{letter}', [AdminPlacementLetterController::class,'show'])->name('letter.show');
 });
