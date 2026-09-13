be841f2ae75bd73bbd1b69700bc1f4b5cd5f6d68
NOFACE_NOMAD <shaikielyn16@gmail.com>
2026-09-13 16:51:22 +0300

    Seed workflow departments and HOD routing

---
+++ database/seeders/WorkflowDepartmentSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Department;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class WorkflowDepartmentSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $departments = [
+            ['name'=>'Computer Science','code'=>'CS','hod'=>'hod.cs@practilink.test'],
+            ['name'=>'Information Technology','code'=>'IT','hod'=>'hod.it@practilink.test'],
+            ['name'=>'Electronics and Telecommunications','code'=>'ECT','hod'=>'hod.ect@practilink.test'],
+        ];
+
+        foreach ($departments as $data) {
+            $department = Department::updateOrCreate(
+                ['code' => $data['code']],
+                ['name' => $data['name']],
+            );
+
+            $hod = User::where('email', $data['hod'])->firstOrFail();
+            $department->users()->syncWithoutDetaching([$hod->id]);
+        }
+    }
+}
