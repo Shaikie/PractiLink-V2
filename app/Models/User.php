98f6696115b14163641639b4c350cda9164e2313
NOFACE_NOMAD <shaikielyn16@gmail.com>
2026-09-13 16:50:18 +0300

    Fix Eloquent attribute import

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Database\Factories\UserFactory;
-use Illuminate\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
