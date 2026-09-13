69433c7e0f4cdaccfee0243831d75661cfbede07
NOFACE_NOMAD <shaikielyn16@gmail.com>
2026-09-13 16:50:22 +0300

    Add department staff relationship

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Department extends Model
 {
     protected $fillable = ['name', 'code'];
+
+    public function applications(): HasMany { return $this->hasMany(Application::class); }
+    public function users(): BelongsToMany { return $this->belongsToMany(User::class, 'department_user'); }
 }
